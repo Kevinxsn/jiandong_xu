b3da25639a835c7ddd4aeda3c46c3890634ec682
Kevinxsn <88812837+Kevinxsn@users.noreply.github.com>
2026-03-07 05:21:05 +0000

    Deploying to gh-pages from @ Kevinxsn/jiandong_xu@341d6537db1a78021e54858ff0463d64ec02eadf 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -9,14 +9,7 @@ ninja.data = [{
     handler: () => {
       window.location.href = "/";
     },
-  },{id: "nav-floating-life-amp-floating-dirt",
-          title: "Floating Life &amp; Floating Dirt",
-          description: "China’s Dog Market is my fourth published book, exploring China’s dog market, its economy, and the social phenomena surrounding it.",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/floating/";
-          },
-        },{id: "nav-china-39-s-dog-market",
+  },{id: "nav-china-39-s-dog-market",
           title: "China&#39;s Dog Market",
           description: "China’s Dog Market is my fourth published book, exploring China’s dog market, its economy, and the social phenomena surrounding it.",
           section: "Navigation",
@@ -29,6 +22,13 @@ ninja.data = [{
           section: "Navigation",
           handler: () => {
             window.location.href = "/A_Brick/";
+          },
+        },{id: "nav-floating-life",
+          title: "Floating Life",
+          description: "China’s Dog Market is my fourth published book, exploring China’s dog market, its economy, and the social phenomena surrounding it.",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/floating/";
           },
         },{id: "nav-徐建东",
           title: "徐建东",
